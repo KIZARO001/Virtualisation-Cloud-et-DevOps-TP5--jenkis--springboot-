9c27fac9cc7058d6cc4e1733e510eae033909b8e
Zakaria Asraoui <zakaria.asraoui>
2025-02-08 13:27:03 +0100

    first commit

---
+++ src/main/java/com/asraoui/jenkisspringboot/JenkisSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.asraoui.jenkisspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkisSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkisSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/asraoui/jenkisspringboot/UserController.java
@@ -0,0 +1,20 @@
+package com.asraoui.jenkisspringboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    @GetMapping("/user")
+    public Map<String, String> getUserInfo() {
+        return Map.of(
+                "nom", "ASRAOUI",
+                "prenom", "ZAKARIA",
+                "email", "z_asraoui@etu.enset-media.ac.ma"
+        );
+    }
+}
